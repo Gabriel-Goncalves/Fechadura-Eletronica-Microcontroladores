b81cdf17ce2d5f3eccc2b6fa1d63b96f9fdf67e0
Gabriel Gonçalves <gabriel_98gm@hotmail.com>
2019-11-19 01:13:43 -0300

    adicionando bibliotecas de dependência

--- criptogafia.h
+++ criptogafia.h
@@ -1,24 +1,26 @@
-#include <stdint.h>
+#include "stdint.h"
 #include <stdio.h> // a libc vai definir essas funções
 // encryption routine 
 
 // encryption routine
-void encrypt (uint32_t* v, uint32_t* k) {
+void encriptar (uint32_t* v, uint32_t* k) {
  uint32_t v0=v[0], v1=v[1], sum=0, i;
  printf("teste %X\n teste 2 %X", v0, v1);
  uint32_t delta=0x9e3779b9;
  uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
  printf("\n\ntesteeee %X\n testeeeeeeeeeeeeeeeee 2 %X\n test %X\n te %X", k0, k1, k2, k3);
+ 
  for (i=0; i < 32; i++) {
- sum += delta;
- v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
- v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+    sum += delta;
+    v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+    v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
  }
+
  v[0]=v0; v[1]=v1;
 }
 
 // decryption routine
-void decrypt (uint32_t* v, uint32_t* k) {
+void decriptar (uint32_t* v, uint32_t* k) {  // não vai existir
  uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i;
  uint32_t delta=0x9e3779b9;
  uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
@@ -32,7 +34,7 @@ void decrypt (uint32_t* v, uint32_t* k) {
 
 
 
-int main() {
+void criptografar() {
   ///uint32_t  *v;
  // v = 0x0123456789abcdef;
  // uint32_t  *k ;
